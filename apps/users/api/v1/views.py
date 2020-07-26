c254b76ab3657cb0efe103d65d3c9795d941a1f5
bindeep.acharya <bindeep.acharya@aayulogic.com>
2020-07-26 19:48:29 +0545

    change viewset

--- apps/users/api/v1/views.py
+++ apps/users/api/v1/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from apps.core.permissions import IsSuperUser
-from apps.core.viewsets import CreateListRetrieveViewSet, ReadOnlyViewSet
+from apps.core.viewsets import CreateListRetrieveViewSet
 from apps.users.api.v1.serializers import (
     UserDetailSerializer,
     CustomTokenObtainPairSerializer, PasswordChangeSerializer,
@@ -22,7 +22,7 @@ class CustomTokenViewBase(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
 
-class UserViewSet(ReadOnlyViewSet):
+class UserViewSet(CreateListRetrieveViewSet):
     serializer_class = UserDetailSerializer
     queryset = USER.objects.filter(is_active=True)
     permission_class_mapper = {
